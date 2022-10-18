216d2b0ebc90aaa90a696c410c4078e37609f3d2
JACLYNHAGGERTY <JACLYNHAGGERTY@GMAIL.com>
2022-10-18 10:15:01 +0200

    finish styling images

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,34 +1,40 @@
 import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
-
-const Content = {
-  height: "700px",
-  display: "inline-flex",
-  float: "left",
-  width: "auto",
-  overflowX: "scroll",
-  img: {
-    borderRadius: "10px"
-  }
-};
-
-const Image = {
-    minWidth: '400px',
-    margin: '10px'
-}
+import "./Gallery.css"
 
 const Gallery = () => {
-    return (
-      <div style={Content}>
-        <StaticImage style={Image} src="../images/Daily.webp" alt="Daily" ></StaticImage>
-        <StaticImage style={Image} src="../images/Sport.webp" alt="Sport" ></StaticImage>
-        <StaticImage style={Image} src="../images/Walk.webp" alt="Walk" ></StaticImage>
-        <StaticImage style={Image} src="../images/Travel.webp" alt="Travel" ></StaticImage>
-        <StaticImage style={Image} src="../images/Recover.webp" alt="Recover" ></StaticImage>
-        <StaticImage style={Image} src="../images/Medical.webp" alt="Medical" ></StaticImage>
-        <StaticImage style={Image} src="../images/Ski.webp" alt="Ski" ></StaticImage>
-      </div>
-    );
-  }
+  return (
+    <div className="content">
+      <a href="#">
+        <StaticImage className="image" src="../images/Daily.webp" alt="Daily" ></StaticImage>
+        <p className="img-text">Daily</p>
+      </a>
+      <a href="#">
+        <StaticImage className="image" src="../images/Sport.webp" alt="Sport" ></StaticImage>
+        <p className="img-text">Sport</p>
+      </a>
+      <a href="#">
+        <StaticImage className="image" src="../images/Walk.webp" alt="Walk" ></StaticImage>
+        <p className="img-text">Walk</p>
+      </a>
+      <a href="#">
+        <StaticImage className="image" src="../images/Travel.webp" alt="Travel" ></StaticImage>
+        <p className="img-text">Travel</p>
+      </a>
+      <a href="#">
+        <StaticImage className="image" src="../images/Recover.webp" alt="Recover" ></StaticImage>
+        <p className="img-text">Recover</p>
+      </a>
+      <a href="#">
+        <StaticImage className="image" src="../images/Medical.webp" alt="Medical" ></StaticImage>
+        <p className="img-text">Medical</p>
+      </a>
+      <a href="#">
+        <StaticImage className="image" src="../images/Ski.webp" alt="Ski" ></StaticImage>
+        <p className="img-text">Ski</p>
+      </a>
+    </div>
+  );
+}
 
 export default Gallery;
